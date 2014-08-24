d1fbf767b28277fdb4b68d6468d0cd67e9c8e9b8
Karl Böhlmark <karl.bohlmark@gmail.com>
2014-08-25 00:07:34 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var Emitter = typeof process == 'undefined' ? require("emitter") : require("component-emitter");
+
+module.exports = observable;
+
+function observable (o) {
+    var wrapper = {}
+    Object.keys(o).forEach(function (key) {
+        Object.defineProperty(wrapper, key, {
+            get: function () {
+                return o[key];
+            },
+            set: function (value) {
+                var oldVal = o[key];
+                o[key] = value
+                wrapper.emit("change " + key, value, oldVal);
+            }
+        })
+    })
+    return wrapper;
+}
